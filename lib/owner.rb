94d2f6fb1287b559f6fdac65aa4ea342e26de332
Jooyon Won <jyon1204@gmail.com>
2019-05-18 19:09:46 -0500

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -5,13 +5,13 @@ class Owner
   attr_accessor :name, :pets, :fish, :cat, :dog
 
   @@all = []
-  @@pets = {:fishes => [], :dogs => [], :cats => []}
 
 
   def initialize(species)
     @species = species
     @name = name
     @@all << self
+    @pets = {:fishes => [], :dogs => [], :cats => []}
   end
 
   def self.count
@@ -31,7 +31,7 @@ class Owner
   end
 
   def pets
-    @@pets
+    @pets
   end
 
   def buy_fish(fish)
@@ -47,31 +47,30 @@ class Owner
   end
 
   def walk_dogs
-    @@pets[:dogs].each do |dog|
+    @pets[:dogs].each do |dog|
       dog.mood = "happy"
     end
   end
 
   def play_with_cats
-    @@pets[:cats].each do |cat|
+    @pets[:cats].each do |cat|
       cat.mood = "happy"
     end
   end
 
   def feed_fish
-    @@pets[:fishes].each do |fish|
+    @pets[:fishes].each do |fish|
       fish.mood = "happy"
     end
   end
 
   def sell_pets
-    pets.each do |type, data|
-      data.map do |state|
-        state.mood = 'nervous'
-      end
-      data.clear
+      selling_pets = {}
+      selling_pets[:dogs] = @pets[:dogs].each {|dog| dog.mood = "nervous"}
+      selling_pets[:cats] = @pets[:cats].each {|cat| cat.mood = "nervous"}
+      selling_pets[:fishes] = @pets[:fishes].each {|fish| fish.mood = "nervous"}
+      @pets = @pets.values.clear
     end
-  end
 
   def list_pets
 	   "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
